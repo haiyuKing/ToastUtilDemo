1f10e8b5131db4f6efea6a457d9dbbe69f83cf8b
haiyuKing <wanghaiyu99999@126.com>
2018-03-27 19:00:46 +0800

    1、优化设置显示位置的功能；

--- app/src/main/java/com/why/project/toastutildemo/util/ToastUtil.java
+++ app/src/main/java/com/why/project/toastutildemo/util/ToastUtil.java
@@ -1,7 +1,9 @@
 package com.why.project.toastutildemo.util;
 
+import android.content.Context;
 import android.view.Gravity;
 import android.widget.Toast;
+
 import com.why.project.toastutildemo.MyApplication;
 
 /**
@@ -22,6 +24,9 @@ public class ToastUtil {
 	        } else {
 	            toast.setText(msg);
 	        }
+			//1、setGravity方法必须放到这里，否则会出现toast始终按照第一次显示的位置进行显示（比如第一次是在底部显示，那么即使设置setGravity在中间，也不管用）
+			//2、虽然默认是在底部显示，但是，因为这个工具类实现了中间显示，所以需要还原，还原方式如下：
+			toast.setGravity(Gravity.BOTTOM, 0, dip2px(MyApplication.getAppContext(),64));
 	        toast.show();
 		}
 	}
@@ -32,10 +37,10 @@ public class ToastUtil {
 		if(MyApplication.getAppContext() != null) {
 			if (toast == null) {
 	            toast = Toast.makeText(MyApplication.getAppContext(), msg, Toast.LENGTH_SHORT);
-	            toast.setGravity(Gravity.CENTER, 0, 0);
 	        } else {
 	            toast.setText(msg);
 	        }
+			toast.setGravity(Gravity.CENTER, 0, 0);
 	        toast.show();
 		}
 	}
@@ -47,10 +52,10 @@ public class ToastUtil {
 		if(MyApplication.getAppContext() != null) {
 			if (toast == null) {
 	            toast = Toast.makeText(MyApplication.getAppContext(), msg, Toast.LENGTH_SHORT);
-	            toast.setGravity(Gravity.TOP, 0, 0);
 	        } else {
 	            toast.setText(msg);
 	        }
+			toast.setGravity(Gravity.TOP, 0, 0);
 	        toast.show();
 		}
 	}
@@ -65,6 +70,7 @@ public class ToastUtil {
 	        } else {
 	            toast.setText(msg);
 	        }
+			toast.setGravity(Gravity.BOTTOM, 0, dip2px(MyApplication.getAppContext(),64));
 	        toast.show();
 		}
 	}
@@ -75,10 +81,10 @@ public class ToastUtil {
 		if(MyApplication.getAppContext() != null) {
 			if (toast == null) {
 	            toast = Toast.makeText(MyApplication.getAppContext(), msg, Toast.LENGTH_LONG);
-	            toast.setGravity(Gravity.CENTER, 0, 0);
 	        } else {
 	            toast.setText(msg);
 	        }
+			toast.setGravity(Gravity.CENTER, 0, 0);
 	        toast.show();
 		}
 	}
@@ -89,11 +95,17 @@ public class ToastUtil {
 		if(MyApplication.getAppContext() != null) {
 			if (toast == null) {
 	            toast = Toast.makeText(MyApplication.getAppContext(), msg, Toast.LENGTH_LONG);
-	            toast.setGravity(Gravity.TOP, 0, 0);
 	        } else {
 	            toast.setText(msg);
 	        }
+			toast.setGravity(Gravity.TOP, 0, 0);
 	        toast.show();
 		}
+	}
+
+	/*=================================常用公共方法============================*/
+	public static int dip2px(Context context, float dpValue) {
+		final float scale = context.getResources().getDisplayMetrics().density;
+		return (int) (dpValue * scale + 0.5f);
 	}
 }
